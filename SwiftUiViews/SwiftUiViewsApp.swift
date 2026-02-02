fe0cc28de404561de85e4d28357bf1b4714ddf23
Ivan Potyng <nekto.ellez@gmail.com>
2026-02-02 19:01:29 +0300

    Initial Commit

---
+++ SwiftUiViews/SwiftUiViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiViewsApp.swift
+//  SwiftUiViews
+//
+//  Created by Nekto_Ellez on 02.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
